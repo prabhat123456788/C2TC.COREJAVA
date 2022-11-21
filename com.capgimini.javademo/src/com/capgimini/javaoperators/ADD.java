f665db1bf36d369e027496e20909d3a56a402e68
Prabhatsingh <Prabhatsingh@Prabhat>
2022-11-21 15:27:45 +0530

    This is my first repositoy

---
+++ com.capgimini.javademo/src/com/capgimini/javaoperators/ADD.java
@@ -0,0 +1,15 @@
+package com.capgimini.javaoperators;
+
+public class ADD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 =2;
+		int add ;
+		add = num1+num2;
+		System.out.println(add);
+
+	}
+
+}
