418768e10cc17d5c83621515e7e8081aefaf6068
Prabhatsingh <Prabhatsingh@Prabhat>
2022-11-23 12:33:29 +0530

    This is My Third Commit

---
+++ com.capgimini.javademo/src/Array/test.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class test {
+
+	public static void main(String[] args) {
+int arr[]=new int[5];
+		
+		arr[0]=4;
+		arr[1]=6;
+		arr[2]=9;
+		arr[3]=2;
+		arr[4]=8;
+		
+		for(int i = 0; i<arr.length ; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
